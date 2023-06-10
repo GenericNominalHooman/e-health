339cfc9c5416b862031c83d3acee2315ba46092d
adzli-necius <61506818+adzli-necius@users.noreply.github.com>
2023-06-10 17:50:33 +0800

    Add new comment

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -218,9 +218,6 @@ if(isset($_GET['logout'])){
                     });
         </script>
 		<?php include(COMPONENTS_DIR . "/footer.php"); ?>
-<!-- nigga -->
-    
- 
     </div>
     <!--Container Main end-->
      <!-- Bootstrap JS Bundle with Popper -->
