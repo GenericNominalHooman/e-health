2862f7e4afa6a17fd4d079064bb67ae6e6a6aa32
GenericNominalHooman <GenericNominal@protonmail.com>
2023-06-16 22:36:20 +0800

    Fix models.php functionality

--- Components/models.php
+++ Components/models.php
@@ -9,20 +9,19 @@ class Models{
 }
 
 class UserModel extends Models{
-    private $connection;
-
     public function __construct($conn){
       // Call parent constructor method
       parent::__construct($conn);
     }
     public function getAllAdmin() {
-        $sql = "SELECT * FROM loginadmin";
-        $result = $this->connection->query($sql);
+        $stmt = $this->conn->prepare("SELECT * FROM loginadmin");
+        $stmt->execute();
+        $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function getAllAdminWhere($columnName, $columnValue) {
-        $stmt = $this->connection->prepare("SELECT * FROM loginadmin WHERE $columnName='$columnValue'");
+        $stmt = $this->conn->prepare("SELECT * FROM loginadmin WHERE $columnName='$columnValue'");
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
