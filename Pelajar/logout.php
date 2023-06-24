ce3805dbc419b0f451fec70e6b724a76dc6d136c
GenericNominalHooman <GenericNominal@protonmail.com>
2023-06-24 16:07:45 +0800

    Update logout.php content to unset all session variables and redirect to landing page

--- Pelajar/logout.php
+++ Pelajar/logout.php
@@ -1,1 +1,12 @@
  
+<?php
+    // Import site config
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/e-health/site_config.php");
+?>
+<?php
+// Remove all session variables
+session_start();
+session_unset();
+// Redirect the user back to index page
+header("Location:".SITE_URL."/index.php");
+?>
