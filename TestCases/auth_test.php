f875b4cc1c2250e99b17778ae0fece589394b9fc
Kamarul-097 <kamarulabrar097@gmail.com>
2023-06-30 23:29:04 +0800

    login pelajar php
    function login pelajar boleh digunakan

--- TestCases/auth_test.php
+++ TestCases/auth_test.php
@@ -6,6 +6,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/e-health/site_config.php");
 // Import auth component
 require_once(COMPONENTS_DIR . "/auth.php");
 
+<<<<<<< Updated upstream
 $dbObj = new Database();
 $authObj = new Auth($dbObj->getConnection());
 
@@ -106,4 +107,31 @@ if (isset($_POST["submit_login"])) {
 <?php
 // Erase all session variables PS: do this only on logout
 // session_unset();
+=======
+    $dbObj = new Database();
+    $authObj = new Auth($dbObj->getConnection());
+?>
+<h2>Login Pelajar</h2>
+<form action="<?php echo($_SERVER["PHP_SELF"]);?>" method="POST">
+    <input type="text" name="nama">
+    <input type="text" name="password">
+    <button type="submit" name="submit">Submit</button>
+</form>
+<?php
+    // Login pelajar
+    if(isset($_POST["submit"])){
+
+        if($authObj->authPelajar($_POST["nama"], $_POST["password"])){
+            // Mesej untuk user yang dah login
+            d($_SESSION["Auth"]); // OK
+            echo("BERJAYA LOG MASUK");
+        }else{
+            // Mesej untuk user yang x berjaya login
+        }
+    }
+?>
+<?php
+    // Erase all session variables PS: do this only on logout
+    session_unset();
+>>>>>>> Stashed changes
 ?>
