c0de3c0c8f07653c81a1b5a884138224cce6ffe9
GenericNominalHooman <GenericNominal@protonmail.com>
2023-07-03 12:16:44 +0800

    Update each users login & registeration pages to follow site's theme

---
+++ Pentadbir/register.php
@@ -0,0 +1,175 @@
+<?php
+// THIS CODE SNIPPET IS REQUIRED ON EVERY PAGE FOR HEADER & FOOTER FUNCTIONALITY TO WORK - Iz
+// Import site settings
+require_once($_SERVER["DOCUMENT_ROOT"] . "/e-health/site_config.php");
+require_once(COMPONENTS_DIR . "/header.php");
+require_once(COMPONENTS_DIR . "/config.php");
+require_once(COMPONENTS_DIR . "/redirect.php");
+require_once(COMPONENTS_DIR . "/auth.php");
+require_once(COMPONENTS_DIR . "/verification.php");
+require_once(TEMPLATE_DIR . "/sidebar2_guest.php");
+require_once(COMPONENTS_DIR . "/header.php");
+?>
+
+<!-- CONTENT BEGIN -->
+<?php
+// Declaring global variables
+$dbObj = new Database();
+$authObj = new Auth($dbObj->getConnection());
+$conn = ($dbObj->getConnection());
+?>
+
+<?php
+// Redirect to profile page if user has already logged on
+if($authObj->isAuth()){
+  $userAuth = $_SESSION["Auth"];
+  switch($userAuth["jenispengguna"]){
+    case "pelajar":
+      Redirect::redirectWithMsg(PELAJAR_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+    case "pentadbir":
+      Redirect::redirectWithMsg(PENTADBIR_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+    case "guru":
+      Redirect::redirectWithMsg(GURU_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+    case "warden":
+      Redirect::redirectWithMsg(WARDEN_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+  }
+}
+?>
+
+<!-- CONTENT BEGIN -->
+<?php
+// Insansiating verification object to determine whether the passed in name is already in used
+$loginModel = new LoginModel($dbObj->getConnection());
+$profilModel = new ProfilModel($dbObj->getConnection());
+$verificationObj = new Verification(null, $loginModel, $profilModel);
+
+if (isset($_POST['submit'])) {
+  // Check whether nama is in used
+  if(!$verificationObj->isNameExist($_POST["namapentadbir"])){
+    //Registering namapentadbir
+    if ($authObj->registerPentadbir($_POST["namapentadbir"], $_POST["katalaluan"], "")) {
+      // pentadbir successfully registered
+      // Login pentadbir
+      $authObj->authPentadbir($_POST["namapentadbir"], $_POST["katalaluan"]);
+      Redirect::redirect(PENTADBIR_URL."/Profil/profil.php");
+    } else {
+      // pentadbir wasn't successfully registered
+      Redirect::redirectWithMsg(PENTADBIR_URL . "/register.php", "Pendaftaran masuk gagal, tolong maklumkan pentadbir sistem berkaitan isu ini.");
+    } 
+  } else {
+    // pentadbir wasn't successfully registered
+    Redirect::redirectWithMsg(PENTADBIR_URL . "/register.php", "Sila gunakan nama lain, nama pentadbir tersebut sudah terdapat di dalam sistem.");
+  }
+}
+?>
+  <section class="gradient-custom">
+    <div class="container py-5">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-12 col-lg-9 col-xl-7">
+          <div class="card  shadow-lg card-registration" style="border-radius: 15px;">
+            <div class="card-body p-4 p-md-5">
+              <!-- MESSAGE HANDLER BACKEND BEGIN -->
+                <!-- Container to display error message -->
+                <div id="errorMessageContainer">
+                </div>
+                <!-- Handle failed login attempt -->
+                <div class="col-md-6 mb-4">
+                    <?php
+                    if (isset($_POST["msg"])) {
+                      $message = $_POST["msg"];
+                      $messageHandlerObj = new MessageHandler();
+                      $messageHandlerObj->addMessage("error", $message);
+                    }
+                    ?>
+                </div>
+              <!-- MESSAGE HANDLER BACKEND ENDS -->
+
+              <div class="container-xxl justify-content-center align-items-center text-center">
+                <div class="row">
+                  <div class="col-12 p-4">
+                    <i class="fa-5x fa-solid fa-user-gear"></i>
+                  </div>
+                </div>
+              </div>
+              <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 text-center">DAFTAR MASUK PENTADBIR</h3>
+
+              <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a href="<?php echo (PENTADBIR_URL . "/register.php"); ?>" class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Daftar Masuk</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a href="<?php echo (PENTADBIR_URL . "/login.php"); ?>" class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab" aria-controls="pills-register" aria-selected="false">Log Masuk</a>
+                </li>
+              </ul>
+
+              <form autocomplete="off" action="register.php" method="post" enctype="multipart/form-data">
+                <div class="form-outline mb-4">
+                  <div class="form-floating">
+                    <input type="text" name="namapentadbir" class="form-control" placeholder="Nama" />
+                    <label for="floatingPassword">Nama</label>
+                  </div>
+                </div>
+
+
+                <!-- Kata Laluan input -->
+                <div class="form-outline mb-4">
+                  <div class="form-floating">
+                    <input type="password" id="password" name="katalaluan" class="form-control" placeholder="Kata Laluan " />
+                    <label class="form-label" for="katalaluan">Kata Laluan</label>
+                    <label for="floatingPassword">Kata Laluan</label>
+                    <div class="mt-3">
+                      <input type="checkbox" onclick="myFunction()">&nbsp;Tunjuk Kata Laluan
+                    </div>
+                  </div>
+                </div>
+                <div class="mb-3">
+
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Daftar </button>
+                <div class="mt-3">
+
+                </div>
+
+              </form>
+              Kembali&nbsp;<a href='<?php echo(SITE_URL."/index.php")?>'>Halaman Utama</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
+  <script src="./js/app.js"></script>
+  <script>
+    function myFunction() {
+      var x = document.getElementById("password");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
+  </script>
+
+<!-- MESSAGE HANDLER FRONTEND BEGIN -->
+<script>
+    let messageHandlerObj = '<?php echo($messageHandlerObj->getMessagesJSON());?>'||null;
+    if(messageHandlerObj!=null){
+      let messageHandlerJSON = new MessageHandlerJSON(messageHandlerObj);
+      messageHandlerJSON.displayMessages("errorMessageContainer");
+    }
+  </script>
+<!-- MESSAGE HANDLER FRONTEND END -->
+<!-- CONTENT END -->
+
+<?php
+  require_once(COMPONENTS_DIR . "/footer.php");
+?>
--- Warden/login.php
+++ Warden/login.php
@@ -1,142 +1,157 @@
 <?php
 // THIS CODE SNIPPET IS REQUIRED ON EVERY PAGE FOR HEADER & FOOTER FUNCTIONALITY TO WORK - Iz
 // Import site settings
-require_once($_SERVER["DOCUMENT_ROOT"]."/e-health/site_config.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/e-health/site_config.php");
 require_once(COMPONENTS_DIR . "/config.php");
-
+require_once(COMPONENTS_DIR . "/redirect.php");
 require_once(COMPONENTS_DIR . "/auth.php");
+require_once(COMPONENTS_DIR . "/message_handler.php");
+require_once(TEMPLATE_DIR . "/sidebar2_guest.php");
+require_once(COMPONENTS_DIR . "/header.php");
+?>
+
+<?php
+// Declaring Global Variables
 $dbObj = new Database();
 $authObj = new Auth($dbObj->getConnection());
 $conn = ($dbObj->getConnection());
 ?>
+
 <?php
-    // Login pelajar
-    if(isset($_POST["submit"])){
-
-        if($authObj->authWarden($_POST["namawarden"], $_POST["katalaluanwarden"])){
-            // Mesej untuk user yang dah login
-            d($_SESSION["Auth"]); // OK
-            echo("BERJAYA LOG MASUK");
-        }else{
-            // Mesej untuk user yang x berjaya login
-
-        }
-    }
-?>
-<?php 
-
-if(isset($_POST['submit'])){
-
-    $namawarden= mysqli_real_escape_string($conn, $_POST['namawarden']);
-    $katalaluan = mysqli_real_escape_string($conn, $_POST['katalaluanwarden']);
- 
-    $select = mysqli_query($conn, "SELECT * FROM `loginwarden` WHERE namawarden  = '$namawarden' AND katalaluanwarden = '$katalaluan'") or die('query failed');
-    if(mysqli_num_rows($select) > 0){
-       $row = mysqli_fetch_assoc($select);
-       $_SESSION['npenggunaguru'] = $namawarden;
-             header("Location: dashboard.php"); // Akan Tukar Nama dashboard iaitu site profile
-           }else{
-             $message[] = 'Salah Kata laluan atau Nama Pengguna';
-          }
-         }
- ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Log Masuk</title>
-
-   <!-- custom css file link  
-   <link rel="stylesheet" href="css/style.css">-->
-   <link rel="shortcut icon" href="images/logo2remove.png" type="image/x-icon">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<style>
-  body{
-    background-color:aliceblue;
+// Redirect to profile page if user has already logged on
+if($authObj->isAuth()){
+  $userAuth = $_SESSION["Auth"];
+  switch($userAuth["jenispengguna"]){
+    case "pelajar":
+      Redirect::redirectWithMsg(PELAJAR_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+    case "pentadbir":
+      Redirect::redirectWithMsg(PENTADBIR_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+    case "guru":
+      Redirect::redirectWithMsg(GURU_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
+    case "warden":
+      Redirect::redirectWithMsg(WARDEN_URL."/Profil/profil.php", "Untuk mengakses halaman daftar masuk, sila log keluar terlebih dahulu.");
+    break;
   }
-  </style>
-
-</head>
-<body>
-
-<section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row justify-content-center align-items-center h-100">
-      <div class="col-12 col-lg-9 col-xl-7">
-        <div class="card shadow-2-strong card-registration" style="border-radius: 15px; border-color:skyblue;">
-          <div class="card-body p-4 p-md-5">
-          <?php
-echo "<img src='" . IMG_URL . "/guru.png' alt='Admin Icon' class='rounded mx-auto d-block' width='200' height='150'>";
+}
 ?>
 
-            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 text-center">LOG MASUK WARDEN</h3>
-            
-            <form action="<?php echo($_SERVER["PHP_SELF"]);?>" method="POST">
-                
-                
-            <?php
-        
-        if(isset($message)){
-           foreach($message as $message){
-              echo '<div class="message alert alert-dark" role="alert">'.$message.'</div>';
-           }
-          }
-        
-     ?>
-                               
-                            
-                            <!-- No K/P input -->
-              
-                            <div class="form-outline mb-4">
-                            <div class="form-floating">
-                              <input type="text" autocomplete="off"  name="namawarden" class="form-control" placeholder="Nama Pengguna" >
-                              <label  for="floatingPassword">Nama Pengguna</label></div>
-                                </div>
+<!-- CONTENT START -->
+<?php
+// Login warden
+if (isset($_POST["submit"])) {
+  //auth perlu ditukar
+  if ($authObj->authWarden($_POST["namawarden"], $_POST["katalaluanwarden"])) {
+    // Redirect user to the profile page
+    Redirect::redirect(WARDEN_URL . "/Profil/profil.php");
+  } else {
+    Redirect::redirectWithMsg(WARDEN_URL . "/login.php", "Kombinasi nama pengguna atau kata laluan yang digunakan adalah salah.");
+  }
+}
+?>
 
-                          <!-- Password input -->
-                             <div class="form-outline mb-4">
-                             <div class="form-floating">
-                                <input type="password" id="password" name="katalaluanwarden" class="form-control" placeholder="Kata Laluan" >
-                                <label class="form-label" for="password">Kata Laluan</label>
-                                <label for="floatingPassword">Kata Laluan</label>
-                                <div class="mt-3">
-                                <input type="checkbox" onclick="myFunction()">&nbsp;Tunjuk Kata Laluan 
-                              </div></div></div>
-                                
-                              
-               
+  <section class="gradient-custom">
+    <div class="container py-5">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-12 col-lg-9 col-xl-7">
+          <div class="card shadow-lg card-registration" style="border-radius: 15px;">
+            <div class="card-body p-4 p-md-5">
+              <!-- MESSAGE HANDLER BACKEND BEGIN -->
+                <!-- Container to display error message -->
+                <div id="errorMessageContainer">
+                </div>
+                <!-- Handle failed login attempt -->
+                <div class="col-md-6 mb-4">
+                    <?php
+                    if (isset($_POST["msg"])) {
+                      $message = $_POST["msg"];
+                      $messageHandlerObj = new MessageHandler();
+                      $messageHandlerObj->addMessage("error", $message);
+                    }
+                    ?>
+                </div>
+              <!-- MESSAGE HANDLER BACKEND ENDS -->
 
-      <!-- Submit button -->
-      <div class="form-outline mb-4">
-      <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Log Masuk</button>
+              <div class="container-xxl justify-content-center align-items-center text-center">
+                <div class="row">
+                  <div class="col-12 p-4">
+                    <i class="fa-5x fa-solid fa-user-tie"></i>
+                  </div>
+                </div>
+              </div>
+              <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 text-center">LOG MASUK WARDEN</h3>
+
+              <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a href="<?php echo (WARDEN_URL . "/register.php"); ?>" class="nav-link" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Daftar Masuk</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a href="<?php echo (WARDEN_URL . "/login.php"); ?>" class="nav-link active" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab" aria-controls="pills-register" aria-selected="false">Log Masuk</a>
+                </li>
+              </ul>
+
+              <form action="<?php echo ($_SERVER["PHP_SELF"]); ?>" method="POST">                
+
+                <!-- No K/P input -->
+
+                <div class="form-outline mb-4">
+                  <div class="form-floating">
+                    <input type="text" autocomplete="off" name="namawarden" class="form-control" placeholder="Nama">
+                    <label for="floatingPassword">Nama</label>
+                  </div>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                  <div class="form-floating">
+                    <input type="password" id="password" name="katalaluanwarden" class="form-control" placeholder="Kata Laluan">
+                    <label class="form-label" for="katalaluanwarden">Kata Laluan</label>
+                    <label for="floatingPassword">Kata Laluan</label>
+                    <div class="mt-3">
+                      <input type="checkbox" onclick="myFunction()">&nbsp;Tunjuk Kata Laluan
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+                <!-- Submit button -->
+                <div class="form-outline mb-4">
+                  <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Log Masuk</button>
+                </div>
+                Kembali&nbsp;<a href='<?php echo(SITE_URL."/index.php")?>'>Halaman Utama</a>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-      Kembali&nbsp;<a href="../index.php">Halaman Utama</a>
-    </form>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<script src= "https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src= "https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
-<script src= "https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
-<script src="./js/app.js"></script>
-<script>
-function myFunction() {
-var x = document.getElementById("password");
-if (x.type === "password") {
-    x.type = "text";
-    } else {
-     x.type = "password";
+    </div>
+  </section>
+  <script>
+    function myFunction() {
+      var x = document.getElementById("password");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
     }
+  </script>
+  
+<!-- MESSAGE HANDLER FRONTEND BEGIN -->
+  <script>
+    let messageHandlerObj = '<?php echo($messageHandlerObj->getMessagesJSON());?>'||null;
+    if(messageHandlerObj!=null){
+      let messageHandlerJSON = new MessageHandlerJSON(messageHandlerObj);
+      messageHandlerJSON.displayMessages("errorMessageContainer");
     }
-</script>
+  </script>
+<!-- MESSAGE HANDLER FRONTEND END -->
+<!-- CONTENT END -->
 
-</body>
-</html>
-<?php include(COMPONENTS_DIR."/footer.php"); ?>
+<?php
+  require_once(COMPONENTS_DIR . "/footer.php"); 
+?>
