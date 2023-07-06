6765a8790055a96f8c2b0f61b01e4baa248bd70a
GenericNominalHooman <GenericNominal@protonmail.com>
2023-07-06 21:43:35 +0800

    Imported auth and models components from new project

--- components/auth.php
+++ components/auth.php
@@ -1,122 +1,71 @@
 <?php
-    // Import site config
-    require_once($_SERVER["DOCUMENT_ROOT"] . "/e-health/site_config.php");
-    // Auth component is dependent on header and config component
-    require_once(COMPONENTS_DIR."/header.php");
-    require_once(COMPONENTS_DIR."/models.php");
-    require_once(COMPONENTS_DIR . "/config.php");
+// Import site config
+require_once($_SERVER["DOCUMENT_ROOT"] . "/e-health/site_config.php");
 ?>
 <?php
-class Auth {
-    private $loginModel;
-    private $registerModel;
-    
-    public function __construct($conn)
-    {
-        require_once(COMPONENTS_DIR."/models.php");
-        $this->loginModel = new LoginModel($conn);
-        $this->registerModel = new RegisterModel($conn);
-    }
-    
-    public function isAuth() {
-        return isset($_SESSION["Auth"]);
-    }
-    
-    public function authPelajar($nama, $katalaluan) {
-        $pelajar = $this->loginModel->getAllPelajarWhere("namapelajar", $nama);
+require_once(COMPONENTS_DIR . "/config.php");
 
-        if(!empty($pelajar)){
-            if(password_verify($katalaluan, $pelajar[0]["katalaluanpelajar"])){
-                if ($pelajar) {
-                    $_SESSION["Auth"] = [
-                        'id' => $pelajar[0]['id'],
-                        'nama' => $pelajar[0]['namapelajar'],
-                        'gambarprofilpelajar' => $pelajar[0]['gambarprofilpelajar'],
-                        'jenispengguna' => "pelajar",
-                    ];
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+class Models
+{
+  protected $conn;
 
-    public function authPentadbir($nama, $katalaluan) {
-        $pentadbir = $this->loginModel->getAllPentadbirWhere("namapentadbir", $nama);
+  public function __construct($conn)
+  {
+    $this->conn = $conn;
+  }
+}
 
-        if(!empty($pentadbir)){
-            if(password_verify($katalaluan, $pentadbir[0]["katalaluanpentadbir"])){
-                if ($pentadbir) {
-                    $_SESSION["Auth"] = [
-                        'id' => $pentadbir[0]['id'],
-                        'nama' => $pentadbir[0]['namapentadbir'],
-                        'gambarprofilpentadbir' => $pentadbir[0]['gambarprofilpentadbir'],
-                        'jenispengguna' => "pentadbir",
-                    ];
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+class UserModel extends Models
+{
+  public function __construct($conn)
+  {
+    // Call parent constructor method
+    parent::__construct($conn);
+  }
 
-    public function authGuru($nama, $katalaluan) {
-        $guru = $this->loginModel->getAllGuruWhere("namaguru", $nama);
-        
-        if(!empty($guru)){
-            if(password_verify($katalaluan, $guru[0]["katalaluanguru"])){
-                if ($guru) {
-                    $_SESSION["Auth"] = [
-                        'id' => $guru[0]['id'],
-                        'nama' => $guru[0]['namaguru'],
-                        'gambarprofilguru' => $guru[0]['gambarprofilguru'],
-                        'jenispengguna' => "guru",
-                    ];
-                    return true;
-                }
-            }
-        }
-        return false;
-        
-    }
+  public function getAllGuruBertugas()
+  {
+    $query = "SELECT * FROM gurubertugas";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-    public function authWarden($nama, $katalaluan) {
-        $warden = $this->loginModel->getAllWardenWhere("namawarden", $nama);
+  public function getAllAdmin()
+  {
+    $query = "SELECT * FROM loginadmin";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-        if(!empty($warden)){
-            if(password_verify($katalaluan, $warden[0]["katalaluanwarden"])){
-                if ($warden) {
-                    $_SESSION["Auth"] = [
-                        'id' => $warden[0]['id'],
-                        'nama' => $warden[0]['namawarden'],
-                        'gambarprofilwarden' => $warden[0]['gambarprofilwarden'],
-                        'jenispengguna' => "warden",
-                    ];
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+  public function getGuruBertugasWhere($columnName, $columnValue){
+    $query = "SELECT * FROM gurubertugas WHERE $columnName='$columnValue'";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-    public function registerPelajar($nama, $katalaluan, $gambarprofilpelajar, $id = null) {
-        $hashed_katalaluan = password_hash($katalaluan, PASSWORD_BCRYPT);
-        return $this->registerModel->createPelajar($id, $nama, $hashed_katalaluan, $gambarprofilpelajar);
-    }
+  public function getAllPelajar()
+  {
+    $query = "SELECT * FROM user_form";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-    public function registerPentadbir($nama, $katalaluan, $gambarprofilpentadbir, $id = null) {
-        $hashed_katalaluan = password_hash($katalaluan, PASSWORD_BCRYPT);
-        return $this->registerModel->createPentadbir($id, $nama, $hashed_katalaluan, $gambarprofilpentadbir);
-    }
+  public function getPelajarWhere($columnName, $columnValue){
+    $query = "SELECT * FROM user_form WHERE $columnName='$columnValue'";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
 
-    public function registerGuru($nama, $katalaluan, $gambarprofilguru, $id = null) {
-        $hashed_katalaluan = password_hash($katalaluan, PASSWORD_BCRYPT);
-        return $this->registerModel->createGuru($id, $nama, $hashed_katalaluan, $gambarprofilguru);
-    }
-
-    public function registerWarden($nama, $katalaluan, $gambarprofilwarden, $id = null) {
-        $hashed_katalaluan = password_hash($katalaluan, PASSWORD_BCRYPT);
-        return $this->registerModel->createWarden($id, $nama, $hashed_katalaluan, $gambarprofilwarden);
-    }
+class ModelsFactory{
+  public function createUserModel(){
+    $dbObj = new Database();
+    return new UserModel($dbObj->getConnection());
+  }
 }
 ?>
