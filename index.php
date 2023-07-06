743be52fc06839c2bc51b91562f7e880c96a1aaa
adzli-necius <61506818+adzli-necius@users.noreply.github.com>
2023-07-06 09:21:57 +0800

    update hospital/index.php
    memasukkan jadual senarai janji temu pelajar pada index.php.

--- index.php
+++ index.php
@@ -2,8 +2,42 @@
 // Site configuration
 require_once("./site_config.php");
 require_once("./components/login_header.php");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital2";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch student appointment data from the database
+$sql = "SELECT nama, nokp, program, tahun, waktu, tarikh, sebab, status FROM janjitemu";
+$result = $conn->query($sql);
 ?>
-<section class="vh-100" style="background-color: aliceblue;">
+<html>
+  <head>
+  <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+
+  </head>
+  <body>
+  <section class="vh-100" style="background-color: aliceblue;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify content align-items-center h-100">
       <!--<div class="col col-xl-10">-->
@@ -70,12 +104,52 @@ require_once("./components/login_header.php");
             </div>
           </div>
           <div class="mb-4">
+          <h2>Senarai Janji Temu Pelajar</h2>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>No Kad Pengenalan</th>
+            <th>Program</th>
+            <th>Tahun</th>
+            <th>Waktu</th>
+            <th>Tarikh</th>
+            <th>Sebab</th>
+
+        </tr>
+        <?php
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["nama"] . "</td>";
+                echo "<td>" . $row["nokp"] . "</td>";
+                echo "<td>" . $row["program"] . "</td>";
+                echo "<td>" . $row["tahun"] . "</td>";
+                echo "<td>" . $row["waktu"] . "</td>";
+                echo "<td>" . $row["tarikh"] . "</td>";
+                echo "<td>" . $row["sebab"] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='6'>No data available</td></tr>";
+        }
+        ?>
+    </table>
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
+<?php
+// Close the database connection
+$conn->close();
+?>
+<?php require_once(COMPONENTS_DIR . "/footer.php"); ?>
+
+  </body>
+  
+</html>
+    
 
 
 <!--<div class="row mt-3">
@@ -112,7 +186,3 @@ alert("WELCOME");
 </div></div>
 
 </div></div>-->
-</BODY>
-
-</HTML>
-<?php require_once(COMPONENTS_DIR . "/footer.php"); ?>
